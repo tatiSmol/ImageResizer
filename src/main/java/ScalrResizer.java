e46410ff06dbbb852797f56caa8714a9979c0f5b
tatiSmol <judeclapton@icloud.com>
2023-11-12 02:19:02 +0300

    ScalrResizer class created

---
+++ src/main/java/ScalrResizer.java
@@ -0,0 +1,58 @@
+import org.imgscalr.Scalr;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ScalrResizer implements Runnable{
+    private File[] files;
+    private int newWidth;
+    private String dstFolder;
+    private long start;
+
+    public ScalrResizer(File[] files, int newWidth, String dstFolder, long start) {
+        this.files = files;
+        this.newWidth = newWidth;
+        this.dstFolder = dstFolder;
+        this.start = start;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (File file : files) {
+                BufferedImage image = ImageIO.read(file);
+                if (image == null) {
+                    continue;
+                }
+
+                int newHeight = (int) Math.round(image.getHeight() / (image.getWidth() / (double) newWidth));
+                BufferedImage newImage = Scalr
+                        .resize(image, Scalr.Method.ULTRA_QUALITY ,newWidth, newHeight, Scalr.OP_ANTIALIAS);
+
+
+//                BufferedImage newImage = new BufferedImage(
+//                        newWidth, newHeight, BufferedImage.TYPE_INT_RGB
+//                );
+
+//                int widthStep = image.getWidth() / newWidth;
+//                int heightStep = image.getHeight() / newHeight;
+//
+//                for (int x = 0; x < newWidth; x++) {
+//                    for (int y = 0; y < newHeight; y++) {
+//                        int rgb = image.getRGB(x * widthStep, y * heightStep);
+//                        newImage.setRGB(x, y, rgb);
+//                    }
+//                }
+
+                File newFile = new File(dstFolder + "/" + file.getName());
+                ImageIO.write(newImage, "jpg", newFile);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println("Finished " + Thread.currentThread().getName()
+                + " after start: " + (System.currentTimeMillis() - start) + "ms");
+    }
+}
